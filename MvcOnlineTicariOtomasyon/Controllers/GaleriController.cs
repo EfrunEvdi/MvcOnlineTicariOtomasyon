e0dcf45ed85a0a23e700a5435f9bc4f26bb32827
Efrun Evdi <efruncetkin@gmail.com>
2023-09-11 18:09:08 +0300

    13. Bolum Sonu

---
+++ MvcOnlineTicariOtomasyon/Controllers/GaleriController.cs
@@ -0,0 +1,23 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class GaleriController : Controller
+    {
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            ViewBag.MainHeader = "Ana Sayfa";
+            ViewBag.MainHeaderUrl = "/Dashboard/Index";
+            ViewBag.SubHeader = "Galeri";
+
+            var values = context.Uruns.Where(x => x.Durum == true).ToList();
+            return View(values);
+        }
+    }
+}
